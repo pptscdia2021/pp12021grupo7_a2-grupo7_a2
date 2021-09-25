c87b3949568df25e5f9f7966de6499540febb2c0
JoaGardiola <joa.gardiola@gmail.com>
2021-09-25 18:31:04 -0300

    gitignore agregado

--- functions.py
+++ functions.py
@@ -3,3 +3,12 @@ def resta(a,b):
 
 def prod(a,b):
     print(a*b)
+
+def division(a,b):
+    print(a/b)
+
+def potencia(a,b):
+    print(a**b)
+
+def suma(a,b):
+    print(a+b)
--- main.py
+++ main.py
@@ -6,3 +6,9 @@ if __name__ == "__main__":
     prod(2,3)
     
     resta(50,25)
+
+    division(100,25)
+
+    potencia(25,3)
+
+    suma(26,25)
