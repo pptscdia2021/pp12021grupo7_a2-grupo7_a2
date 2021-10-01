aa1c1807eeb17fb0c3ed3033930f53264ff9916f
JoaGardiola <joa.gardiola@gmail.com>
2021-10-01 17:55:08 -0300

    Separe proyecto 1 en carpeta

---
+++ TP1-Funciones/README.md
@@ -0,0 +1,2 @@
+# pp12021grupo7_a2-grupo7_a2
+pp12021grupo7_a2-grupo7_a2 created by GitHub Classroom
---
+++ TP1-Funciones/functions.py
@@ -0,0 +1,14 @@
+def resta(a,b):
+    print(a-b)
+
+def prod(a,b):
+    print(a*b)
+
+def division(a,b):
+    print(a/b)
+
+def potencia(a,b):
+    print(a**b)
+
+def suma(a,b):
+    print(a+b)
